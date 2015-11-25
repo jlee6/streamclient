3316c5fe9abb511402a9f5ebc5ed96f8b8e6a00c
joon_lee <jlee@novetta.com>
2015-11-24 23:27:23 -0500

    Imported additional dependencies

--- app/src/main/java/com/jlee/mobile/stream/ClientActivity.java
+++ app/src/main/java/com/jlee/mobile/stream/ClientActivity.java
@@ -1,5 +1,6 @@
 package com.jlee.mobile.stream;
 
+import android.animation.LayoutTransition;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ClientActivity extends AppCompatActivity {
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.search_bar)
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        searchView.setLayoutTransition(new LayoutTransition());
     }
 
     @Override
@@ -48,5 +54,11 @@ public class ClientActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab)
+    void onShowFabSnackbar(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
